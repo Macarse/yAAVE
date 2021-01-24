9ad37b4fcded2d05f0d73a54b92304487bd5fca4
Sam Priestley <sfpriestley@hotmail.co.uk>
2021-01-24 14:50:33 +0000

    bare bones

---
+++ tests/conftest.py
@@ -0,0 +1,139 @@
+import pytest
+from brownie import config
+
+@pytest.fixture
+def andre(accounts):
+    # Andre, giver of tokens, and maker of yield
+    yield accounts[0]
+
+@pytest.fixture
+def currency(interface):
+    #this one is curvesteth
+    yield interface.ERC20('0x06325440D014e39736583c165C2963BA99fAf14E')
+
+@pytest.fixture
+def whale(accounts, web3, currency, chain):
+    #big binance7 wallet
+    #acc = accounts.at('0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8', force=True)
+    #big binance8 wallet
+    acc = accounts.at('0x4AfD924260902956800B8E6B964Fdabf24C0AD7C', force=True)
+
+    assert currency.balanceOf(acc)  > 0
+    
+    yield acc
+
+@pytest.fixture
+def samdev(accounts):
+    #big binance7 wallet
+    #acc = accounts.at('0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8', force=True)
+    #big binance8 wallet
+    acc = accounts.at('0xC3D6880fD95E06C816cB030fAc45b3ffe3651Cb0', force=True)
+
+
+    
+    yield acc
+
+@pytest.fixture
+def devms(accounts):
+    acc = accounts.at('0x846e211e8ba920B353FB717631C015cf04061Cc9', force=True)
+    yield acc
+
+@pytest.fixture
+def token(andre, Token):
+    yield andre.deploy(Token)
+
+
+@pytest.fixture
+def gov(accounts):
+    # yearn multis... I mean YFI governance. I swear!
+    yield accounts[1]
+
+
+@pytest.fixture
+def rewards(gov):
+    yield gov  # TODO: Add rewards contract
+
+
+@pytest.fixture
+def guardian(accounts):
+    # YFI Whale, probably
+    yield accounts[2]
+
+
+@pytest.fixture
+def vault(pm, gov, rewards, guardian, currency):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = gov.deploy(Vault)
+    vault.initialize(currency, gov, rewards, "", "", guardian)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    yield vault
+
+
+@pytest.fixture
+def strategist(accounts):
+    # You! Our new Strategist!
+    yield accounts[3]
+
+
+@pytest.fixture
+def keeper(accounts):
+    # This is our trusty bot!
+    yield accounts[4]
+
+
+@pytest.fixture
+def strategy(strategist, keeper, vault, Strategy):
+    strategy = strategist.deploy(Strategy, vault)
+    strategy.setKeeper(keeper)
+    yield strategy
+
+
+
+@pytest.fixture
+def nocoiner(accounts):
+    # Has no tokens (DeFi is a ponzi scheme!)
+    yield accounts[5]
+
+
+@pytest.fixture
+def pleb(accounts, andre, token, vault):
+    # Small fish in a big pond
+    a = accounts[6]
+    # Has 0.01% of tokens (heard about this new DeFi thing!)
+    bal = token.totalSupply() // 10000
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
+    yield a
+
+
+@pytest.fixture
+def chad(accounts, andre, token, vault):
+    # Just here to have fun!
+    a = accounts[7]
+    # Has 0.1% of tokens (somehow makes money trying every new thing)
+    bal = token.totalSupply() // 1000
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
+    yield a
+
+
+@pytest.fixture
+def greyhat(accounts, andre, token, vault):
+    # Chaotic evil, will eat you alive
+    a = accounts[8]
+    # Has 1% of tokens (earned them the *hard way*)
+    bal = token.totalSupply() // 100
+    token.transfer(a, bal, {"from": andre})
+    # Unlimited Approvals
+    token.approve(vault, 2 ** 256 - 1, {"from": a})
+    # Deposit half their stack
+    vault.deposit(bal // 2, {"from": a})
+    yield a
+
+
---
+++ tests/test_operation.py
@@ -0,0 +1,18 @@
+from itertools import count
+from brownie import Wei, reverts
+import eth_abi
+from brownie.convert import to_bytes
+from useful_methods import genericStateOfStrat,genericStateOfVault
+import random
+import brownie
+
+# TODO: Add tests here that show the normal operation of this strategy
+#       Suggestions to include:
+#           - strategy loading and unloading (via Vault addStrategy/revokeStrategy)
+#           - change in loading (from low to high and high to low)
+#           - strategy operation at different loading levels (anticipated and "extreme")
+
+def test_ops(currency,strategist, interface):
+    
+
+   
